4459ecdfedb521e7f250ee6af7a66d5150e8578f
Sachin Kumar <skofficial116@gmail.com>
2025-03-08 12:25:01 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-// import mongoose, { mongo } from 'mongoose'
-// import { DB_NAME } from './constants'
-
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
-
+// import app from "./utils/app.js"
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    // console.log('Ser');
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+
+    })
+
+})
+.catch((err) => {
+    console.log('Server Error: ' + err);
+})
 
 
 
