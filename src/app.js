c0020d649097f2dc60af3748ca57a2efb725ac25
Sachin Kumar <skofficial116@gmail.com>
2025-03-17 12:12:00 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -3,10 +3,18 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: (origin, callback) => {
+        callback(null, origin || "*");  // Allow all origins
+    },
     credentials: true
-}))
+}));
+
 
 app.use(express.json({ limit: "16kb" }))
 app.use(cookieParser())
