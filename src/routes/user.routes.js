6ae6c8fe6d8c13da2f478e0ec30e24f1d3d12fcc
Sachin Kumar <skofficial116@gmail.com>
2025-03-17 11:45:03 +0530

    remove multer

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,17 +4,18 @@ import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        {
-            name: 'avatar',
-            maxCount: 1
-        },
-        {
-            name: 'coverImage',
-            maxCount: 1
-        }
-    ]), registerUser);
+router.route("/register").post(registerUser);
+// router.route("/register").post(
+//     upload.fields([
+//         {
+//             name: 'avatar',
+//             maxCount: 1
+//         },
+//         {
+//             name: 'coverImage',
+//             maxCount: 1
+//         }
+//     ]), registerUser);
 
 router.route("/login").post(loginUser)
 //secured routes
