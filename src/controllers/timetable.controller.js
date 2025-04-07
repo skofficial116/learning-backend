d4adab1005cef7fb2fcce72c1690733d2b4c5043
Sachin <your_email@example.com>
2025-04-07 20:02:40 +0530

    added timetable and faculty json and updated user routes

---
+++ src/controllers/timetable.controller.js
@@ -0,0 +1,9 @@
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+
+
+function getStudentTimeTable(course, semester, section){
+
+    
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,20 +1,9 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser, accessRefreshToken } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        {
-            name: 'avatar',
-            maxCount: 1
-        },
-        {
-            name: 'coverImage',
-            maxCount: 1
-        }
-    ]), registerUser);
+router.route("/register").post( registerUser);
 
 router.route("/login").post(loginUser)
 //secured routes
